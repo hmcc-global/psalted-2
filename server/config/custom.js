5862d0567537f0ef649c51f60ac434cdc2145710
Tamara Yustian <tamara.yustian@premialab.com>
2023-08-14 14:17:44 +0800

    Add more configs

--- server/config/custom.js
+++ server/config/custom.js
@@ -9,14 +9,26 @@
  */
 
 module.exports.custom = {
+  /**************************************************************************
+   *                                                                         *
+   * The base URL to use during development.                                 *
+   *                                                                         *
+   * • No trailing slash at the end                                          *
+   * • `http://` or `https://` at the beginning.                             *
+   *                                                                         *
+   * > This is for use in custom logic that builds URLs.                     *
+   * > It is particularly handy for building dynamic links in emails,        *
+   * > but it can also be used for user-uploaded images, webhooks, etc.      *
+   *                                                                         *
+   **************************************************************************/
+  baseUrl: "http://localhost:3000",
 
   /***************************************************************************
-  *                                                                          *
-  * Any other custom config this Sails app should use during development.    *
-  *                                                                          *
-  ***************************************************************************/
+   *                                                                          *
+   * Any other custom config this Sails app should use during development.    *
+   *                                                                          *
+   ***************************************************************************/
   // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
   // stripeSecret: 'sk_test_Zzd814nldl91104qor5911gjald',
   // …
-
 };

7b8edb3a6a1ca2fa8ca3c3d3dec9e24fc8a01259
Richie <ricia1212@gmail.com>
2023-09-18 02:22:07 +0800

    GH-1: remove any typing

--- server/src/models/setlist.model.ts
+++ server/src/models/setlist.model.ts
@@ -3,23 +3,28 @@ import { Schema, model, models, Types } from 'mongoose';
 interface ISetlist {
   name: string;
   date: string;
-  createdBy: string;
+  createdBy: Types.ObjectId;
   songs: Types.Array<Types.ObjectId>;
-  lastUpdatedBy: string;
+  lastUpdatedBy: Types.ObjectId;
   publicLink: string;
   groupIds: Types.Array<Types.ObjectId>;
 }
 
-const setlistSchema = new Schema<ISetlist>({
-  name: { type: String, required: true },
-  date: { type: String, required: true },
-  createdBy: { type: String },
-  songs: [{ type: Types.ObjectId, ref: 'Song' }],
-  lastUpdatedBy: { type: String },
-  publicLink : { type: String, required: true, unique: true },
-  groupIds: [{ type: Types.ObjectId, ref: 'Group' }],
-});
+const setlistSchema = new Schema<ISetlist>(
+  {
+    name: { type: String, required: true },
+    date: { type: String, required: true },
+    createdBy: { type: Schema.Types.ObjectId, ref: 'User' },
+    songs: [{ type: Types.ObjectId, ref: 'Song' }],
+    lastUpdatedBy: { type: Schema.Types.ObjectId, ref: 'User' },
+    publicLink: { type: String, required: true, unique: true },
+    groupIds: [{ type: Types.ObjectId, ref: 'Group' }],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Setlist = models.Setlist || model<ISetlist>('Setlist', setlistSchema);
 
-export default Setlist;
+export { Setlist, ISetlist };

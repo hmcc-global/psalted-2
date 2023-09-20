0cc85a4beeef197b7e0157d369a1f7d79a3fffb1
Tamara Yustian <tamarayustian@gmail.com>
2023-09-20 18:02:30 +0800

    GH-3: Add group controllers to routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import userRouter from './user.routes';
+import groupRouter from './group.routes'
 
 const getRoutes = (): Router => {
   const router = Router();
 
   router.use('/users', userRouter);
+  router.use('/groups', groupRouter)
 
   return router;
 };

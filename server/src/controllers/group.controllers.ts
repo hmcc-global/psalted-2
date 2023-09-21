903c1ce54333d7c506ab5a983a42d41782e03caa
Tamara Yustian <tamarayustian@gmail.com>
2023-09-21 10:49:19 +0800

    GH-3: Add comments

--- server/src/controllers/group.controllers.ts
+++ server/src/controllers/group.controllers.ts
@@ -8,6 +8,8 @@ const sendResponse = (res: Response, statusCode: number, payload: IGroup[] | IGr
 const createGroup: RequestHandler = async (req: Request, res: Response) => {
   const { ...toCreate } = req.body;
 
+  // Check if any fields are present. MongoDB will throw an error
+  // if no fields are present or if a required field is missing
   if (Object.keys(toCreate).length > 0) {
     try {
       const data = await Group.create(toCreate);
@@ -20,14 +22,14 @@ const createGroup: RequestHandler = async (req: Request, res: Response) => {
     } catch (error: any) {
       sendResponse(res, 500, error.message);
     }
-  } else {
-    sendResponse(res, 400, 'Missing required fields');
   }
+  sendResponse(res, 400, 'Missing required fields');
 };
 
 const getGroup: RequestHandler = async (req: Request, res: Response) => {
   const { id: groupId } = req.params;
 
+  // Get group by id
   if (groupId) {
     try {
       const data = await Group.findOne({ _id: groupId });
@@ -41,8 +43,9 @@ const getGroup: RequestHandler = async (req: Request, res: Response) => {
       sendResponse(res, 500, error.message);
     }
   }
+  // If no id, get all groups
   try {
-    const data = await Group.find().exec();
+    const data = await Group.find();
 
     if (data) {
       sendResponse(res, 200, data);
@@ -57,6 +60,7 @@ const getGroup: RequestHandler = async (req: Request, res: Response) => {
 const updateGroup: RequestHandler = async (req: Request, res: Response) => {
   const { id: groupId, ...toUpdate } = req.body;
 
+  // Update group by id with fields to update
   if (groupId && Object.keys(toUpdate).length > 0) {
     try {
       const updatedGroup = await Group.updateOne({ _id: groupId }, { $set: toUpdate });
@@ -69,14 +73,14 @@ const updateGroup: RequestHandler = async (req: Request, res: Response) => {
     } catch (error: any) {
       sendResponse(res, 500, error.message);
     }
-  } else {
-    sendResponse(res, 400, 'Missing required fields');
   }
+  sendResponse(res, 400, 'Missing required fields');
 };
 
 const deleteGroup: RequestHandler = async (req: Request, res: Response) => {
   const { id: groupId } = req.params;
 
+  // Soft delete group by id
   if (groupId) {
     try {
       await Group.updateOne({ _id: groupId }, { $set: { isDeleted: true } });
@@ -86,6 +90,7 @@ const deleteGroup: RequestHandler = async (req: Request, res: Response) => {
       sendResponse(res, 500, error.message);
     }
   }
+  sendResponse(res, 400, 'Missing required fields');
 };
 
 export { createGroup, getGroup, updateGroup, deleteGroup };

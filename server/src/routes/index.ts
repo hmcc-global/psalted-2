6c2bbf189a1ceb8e37802be8ab2331980af81710
Yoo Yuen Yau <yuenyau989@hotmail.com>
2023-09-21 23:31:40 +0800

    Revert "GH-2: song api and model"
    This reverts commit 6aaaf9bcd030245296ad4db883e33c43f2d2c253.

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express';
 
 import userRouter from './user.routes';
-import songRouter from './song.routes';
 
 const getRoutes = (): Router => {
   const router = Router();
 
   router.use('/users', userRouter);
-  router.use('/songs', songRouter);
 
   return router;
 };

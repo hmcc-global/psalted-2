193412d0717657b4894575caf5a8413ee6d450f4
Yoo Yuen Yau <yuenyau989@hotmail.com>
2023-11-25 23:50:36 +0800

    GH-21: Removed persist gate due to rendering issue

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,29 +1,25 @@
 import { Provider } from 'react-redux';
-import { store, persistor } from './store';
+import { store } from './store';
 import { CssBaseline, Box } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import { routes as appRoutes } from './routes';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import customTheme from './theme';
-import { PersistGate } from 'redux-persist/integration/react';
-
 function App() {
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <ThemeProvider theme={customTheme}>
-          <CssBaseline />
-          <Box height="100vh" display="flex" flexDirection="column">
-            <BrowserRouter>
-              <Routes>
-                {appRoutes.map((route) => (
-                  <Route key={route.key} path={route.path} element={<route.component />} />
-                ))}
-              </Routes>
-            </BrowserRouter>
-          </Box>
-        </ThemeProvider>
-      </PersistGate>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline />
+        <Box height="100vh" display="flex" flexDirection="column">
+          <BrowserRouter>
+            <Routes>
+              {appRoutes.map((route) => (
+                <Route key={route.key} path={route.path} element={<route.component />} />
+              ))}
+            </Routes>
+          </BrowserRouter>
+        </Box>
+      </ThemeProvider>
     </Provider>
   );
 }

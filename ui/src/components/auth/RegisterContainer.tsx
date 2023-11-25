23ef06964678beb321670b33fac60f0820f2d184
Yoo Yuen Yau <yuenyau989@hotmail.com>
2023-11-25 23:00:34 +0800

    GH-21: Register Page and Auth Routes

---
+++ ui/src/components/auth/RegisterContainer.tsx
@@ -0,0 +1,172 @@
+import React, { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import axios from 'axios';
+import { Box, Button, Link, Stack, TextField, Typography } from '@mui/material';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import { RegisterFormFields } from './helpers/form.types';
+import { formSpacing, formWidth } from './helpers/constants';
+
+// zod validation
+const registerValidationSchema = z
+  .object({
+    fullName: z.string().trim().min(1, { message: 'Please enter your full name' }).max(100),
+    email: z.string().email({ message: 'Please enter a valid email' }).trim(),
+    password: z
+      .string()
+      .trim()
+      .min(6, { message: 'Password must be at least 6 characters long' })
+      .max(100, { message: 'Password must be at most 100 characters long' }),
+    confirmPassword: z.string().trim(),
+  })
+  .refine(
+    (values) => {
+      return values.password === values.confirmPassword;
+    },
+    {
+      message: 'The passwords do not match',
+      path: ['confirmPassword'],
+    }
+  );
+
+const LoginContainer: React.FC = (props: any) => {
+  const { history } = props;
+
+  const { register, handleSubmit, formState } = useForm<RegisterFormFields>({
+    resolver: zodResolver(registerValidationSchema),
+  });
+
+  const { errors } = formState;
+
+  const [invalidResgistration, setInvalidRegistration] = useState('');
+
+  const handleSignup: SubmitHandler<RegisterFormFields> = async (data) => {
+    console.log(data);
+    try {
+      const payload = await axios.post('/api/auth/signup', {
+        fullName: data.fullName,
+        email: data.email,
+        password: data.password ? data.password : '',
+      });
+      if (payload.status === 200) {
+        setTimeout(() => {
+          history.push('/login');
+        }, 3000);
+      }
+    } catch (err: any) {
+      if (err.response && err.response.status === 409) {
+        console.log('Email already exists');
+        setInvalidRegistration('Email already exists, please try with another email');
+      } else if (err.response && err.response.status === 422) {
+        console.log('Required fields not filled');
+        setInvalidRegistration('Required fields not filled');
+      }
+    }
+  };
+
+  return (
+    <>
+      <Box
+        minHeight={'100vh'}
+        sx={{
+          backgroundImage: `url(${process.env.PUBLIC_URL}/images/psalted-bg.jpg)`,
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          backgroundAttachment: 'fixed',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Box margin={'auto'} bgcolor={'transparent'} width={formWidth}>
+          <Stack direction={'column'} margin={'auto'} spacing={formSpacing}>
+            <Stack spacing={1}>
+              <Typography variant="h1" color={'primary'} textAlign={'center'} fontWeight={'bold'}>
+                REGISTER
+              </Typography>
+              <Typography variant={'body1'} textAlign={'center'}>
+                Create a New Account
+              </Typography>
+            </Stack>
+            <form onSubmit={handleSubmit(handleSignup)}>
+              <Stack spacing={formSpacing}>
+                <Stack spacing={1}>
+                  <TextField
+                    label="Full Name"
+                    fullWidth
+                    {...register('fullName', {
+                      required: 'Required',
+                    })}
+                  />
+                  {errors.fullName && (
+                    <Typography variant={'body2'} color={'error'}>
+                      {errors.fullName?.message}
+                    </Typography>
+                  )}
+                </Stack>
+                <Stack spacing={1}>
+                  <TextField
+                    label="Email"
+                    fullWidth
+                    autoComplete={'email'}
+                    {...register('email', {
+                      required: 'Required',
+                    })}
+                  />
+                  {errors.email && (
+                    <Typography variant={'body2'} color={'error'}>
+                      {errors.email?.message}
+                    </Typography>
+                  )}
+                </Stack>
+                <Stack spacing={1}>
+                  <TextField
+                    label="Password"
+                    type="password"
+                    {...register('password', {
+                      required: 'Required',
+                    })}
+                    fullWidth
+                  />
+                  {errors.password && (
+                    <Typography variant={'body2'} color={'error'}>
+                      {errors.password?.message}
+                    </Typography>
+                  )}
+                </Stack>
+                <Stack spacing={1}>
+                  <TextField
+                    label="Confirm Password"
+                    type="password"
+                    {...register('confirmPassword', {
+                      required: 'Required',
+                    })}
+                    fullWidth
+                  />
+                  {errors.confirmPassword && (
+                    <Typography variant={'body2'} color={'error'}>
+                      {errors.confirmPassword?.message}
+                    </Typography>
+                  )}
+                </Stack>
+                {invalidResgistration ? (
+                  <Typography variant={'body2'} color={'error'}>
+                    {invalidResgistration}
+                  </Typography>
+                ) : null}
+                <Button type={'submit'} color={'primary'} variant={'contained'} fullWidth>
+                  REGISTER
+                </Button>
+                <Link href="/login" underline={'hover'} variant="button" textAlign={'center'}>
+                  ALREADY HAVE AN ACCOUNT? LOGIN
+                </Link>
+              </Stack>
+            </form>
+          </Stack>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default LoginContainer;
--- ui/src/routes.ts
+++ ui/src/routes.ts
@@ -1,5 +1,9 @@
 import { FC } from 'react';
 import HomeContainer from './components/home/HomeContainer';
+import LoginContainer from './components/auth/LoginContainer';
+import RegisterContainer from './components/auth/RegisterContainer';
+import RecoverPasswordContainer from './components/auth/RecoverPassword';
+import ResetPasswordContainer from './components/auth/ResetPasswordContainer';
 
 // interface
 interface Route {
@@ -17,5 +21,33 @@ export const routes: Array<Route> = [
     path: '/',
     enabled: true,
     component: HomeContainer,
+  },
+  {
+    key: 'login-route',
+    title: 'Login',
+    path: '/login',
+    enabled: true,
+    component: LoginContainer,
+  },
+  {
+    key: 'register-route',
+    title: 'Register',
+    path: '/register',
+    enabled: true,
+    component: RegisterContainer,
+  },
+  {
+    key: 'recover-password-route',
+    title: 'Recover Password',
+    path: '/password/recover',
+    enabled: true,
+    component: RecoverPasswordContainer,
+  },
+  {
+    key: 'reset-password-route',
+    title: 'Reset Password',
+    path: '/password/reset',
+    enabled: true,
+    component: ResetPasswordContainer,
   },
 ];

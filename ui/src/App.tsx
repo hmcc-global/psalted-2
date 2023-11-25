77f6a67411a86464ec1f40c434d42b13da84c711
Yoo Yuen Yau <yuenyau989@hotmail.com>
2023-11-25 22:56:59 +0800

    GH-21: Added customTheme and redux persist gate

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,42 +1,29 @@
 import { Provider } from 'react-redux';
-import { store } from './store';
+import { store, persistor } from './store';
 import { CssBaseline, Box } from '@mui/material';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
 import { routes as appRoutes } from './routes';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import customTheme from './theme';
+import { PersistGate } from 'redux-persist/integration/react';
 
 function App() {
-  const theme = createTheme({
-    palette: {
-      primary: {
-        light: '#717CFF',
-        main: '#717CFF',
-        dark: '#717CFF',
-        contrastText: '#000',
-      },
-      secondary: {
-        main: '#717CFF',
-        light: '#717CFF',
-        dark: '#717CFF',
-        contrastText: '#000',
-      },
-    },
-  });
-
   return (
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Box height="100vh" display="flex" flexDirection="column">
-          <BrowserRouter>
-            <Routes>
-              {appRoutes.map((route) => (
-                <Route key={route.key} path={route.path} element={<route.component />} />
-              ))}
-            </Routes>
-          </BrowserRouter>
-        </Box>
-      </ThemeProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider theme={customTheme}>
+          <CssBaseline />
+          <Box height="100vh" display="flex" flexDirection="column">
+            <BrowserRouter>
+              <Routes>
+                {appRoutes.map((route) => (
+                  <Route key={route.key} path={route.path} element={<route.component />} />
+                ))}
+              </Routes>
+            </BrowserRouter>
+          </Box>
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   );
 }

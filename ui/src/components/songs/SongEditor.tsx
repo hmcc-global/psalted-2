25f97428be134337d6eccc5206faa8d8a232eadc
Tamara Yustian <tamarayustian@gmail.com>
2023-11-29 02:25:00 +0800

    GH-24: Create a working form

--- ui/src/components/songs/SongEditor.tsx
+++ ui/src/components/songs/SongEditor.tsx
@@ -15,18 +15,23 @@ import {
 import { Info, LibraryMusic, Edit, ArrowBackIosNew, Add } from '@mui/icons-material';
 import { PRIMARY_MAIN } from '../../theme';
 import { musicKeysOptions, tempoOptions } from '../../constants';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { SongEditorFields } from '#/types/song.types';
+import axios from 'axios';
 
 interface SongEditorProps {
   actionOnEditor: string;
 }
 
 const SongEditor: FC<SongEditorProps> = ({ actionOnEditor }) => {
-  const [theme, setTheme] = useState<string[]>([]);
-  const [tempo, setTempo] = useState<string[]>([]);
-  const [recommendedKeys, setRecommendedKeys] = useState<string[]>([]);
+  const [invalidSong, setInvalidSong] = useState<string>('');
+
+  const { register, handleSubmit, formState } = useForm<SongEditorFields>();
+  const { errors } = formState;
 
   const themeOptions: string[] = [];
 
+  // either ADD NEW or EDIT. default is ADD NEW
   const titleEditor = (actionOnEditor: string): ReactElement => {
     return (
       <Typography
@@ -37,7 +42,6 @@ const SongEditor: FC<SongEditorProps> = ({ actionOnEditor }) => {
         gap={1}
         mx={2}
       >
-        {/* either ADD NEW or EDIT. default is ADD NEW */}
         {actionOnEditor === 'EDIT' ? (
           <Edit sx={{ color: PRIMARY_MAIN }} />
         ) : (
@@ -48,100 +52,186 @@ const SongEditor: FC<SongEditorProps> = ({ actionOnEditor }) => {
     );
   };
 
+  const handleSaveSong: SubmitHandler<SongEditorFields> = async (data) => {
+    try {
+      const payload = await axios.post('/api/songs/create', {
+        artist: data.artist,
+        title: data.title,
+        themes: data.themes,
+        tempo: data.tempo,
+        year: data.year,
+        code: data.code,
+        originalKey: data.originalKey,
+        recommendedKeys: data.recommendedKeys,
+        chordLyrics: data.chordLyrics,
+      });
+
+      if (payload.status === 200) {
+        console.log('Successfully saved song!');
+        setInvalidSong('');
+        return payload.data;
+      }
+      console.log('Error saving song!');
+      setInvalidSong('Error saving song!');
+      return;
+    } catch (error: any) {
+      setInvalidSong(error.response.data);
+      console.log(error);
+    }
+  };
+
   return (
     <Container>
-      <Toolbar>
-        <ArrowBackIosNew sx={{ color: PRIMARY_MAIN }} />
-        {titleEditor(actionOnEditor)}
-        <Button type={'submit'} color={'primary'} variant={'contained'} sx={{ marginLeft: 'auto' }}>
-          SAVE
-        </Button>
-      </Toolbar>
-      <Divider />
+      <form onSubmit={handleSubmit(handleSaveSong)}>
+        <Toolbar>
+          <ArrowBackIosNew sx={{ color: PRIMARY_MAIN }} />
+          {titleEditor(actionOnEditor)}
+          <Button
+            type={'submit'}
+            color={'primary'}
+            variant={'contained'}
+            sx={{ marginLeft: 'auto' }}
+          >
+            SAVE
+          </Button>
+        </Toolbar>
+        <Divider />
 
-      <Box my={2}>
-        <Stack direction={['row']} spacing={8}>
-          {/* column 1 */}
-          <Box width={'30vw'}>
-            <Stack direction="column" spacing={2}>
-              <Typography variant="h4" sx={{ display: 'flex', alignItems: 'center' }} gap={1}>
-                <Info sx={{ color: '#4B50B4' }} />
-                Song Details
-              </Typography>
+        <Box my={2}>
+          {/* Error message */}
+          {invalidSong ? (
+            <Typography variant={'body2'} color={'error'}>
+              {invalidSong}
+            </Typography>
+          ) : null}
 
-              <TextField id="artist" label="Artist Name" />
-              <TextField id="song_title" label="Song Title" />
+          <Stack direction={['row']} spacing={8}>
+            {/* column 1: Song Details */}
+            <Box width={'30vw'}>
+              <Stack direction="column" spacing={2}>
+                <Typography variant="h4" sx={{ display: 'flex', alignItems: 'center' }} gap={1}>
+                  <Info sx={{ color: '#4B50B4' }} />
+                  Song Details
+                </Typography>
 
-              {/* Themes field */}
-              <FormControl fullWidth>
-                <Autocomplete
-                  multiple
-                  id="tags-outlined"
-                  options={themeOptions}
-                  getOptionLabel={(option) => option}
-                  filterSelectedOptions
-                  renderInput={(params) => (
-                    <TextField {...params} variant="outlined" label="Theme" />
-                  )}
+                <TextField
+                  id="artist"
+                  label="Artist Name"
+                  {...register('artist', { required: 'Required' })}
                 />
-              </FormControl>
-
-              {/* Tempo field */}
-              <FormControl fullWidth>
-                <Autocomplete
-                  multiple
-                  id="tags-outlined"
-                  options={tempoOptions}
-                  getOptionLabel={(option) => option}
-                  filterSelectedOptions
-                  renderInput={(params) => (
-                    <TextField {...params} variant="outlined" label="Tempo" />
-                  )}
+                <TextField
+                  id="title"
+                  label="Song Title"
+                  {...register('title', { required: 'Required' })}
                 />
-              </FormControl>
 
-              {/* Original Key field */}
-              <FormControl fullWidth>
-                <Autocomplete
-                  id="original-key"
-                  options={musicKeysOptions}
-                  getOptionLabel={(option) => option}
-                  filterSelectedOptions
-                  renderInput={(params) => (
-                    <TextField {...params} variant="outlined" label="Original Key" />
-                  )}
+                {/* Themes field */}
+                <FormControl fullWidth>
+                  <Autocomplete
+                    multiple
+                    id="tags-outlined"
+                    options={themeOptions}
+                    getOptionLabel={(option) => option}
+                    filterSelectedOptions
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        variant="outlined"
+                        label="Theme"
+                        {...register('themes', { required: 'Required' })}
+                      />
+                    )}
+                  />
+                </FormControl>
+
+                {/* Tempo field */}
+                <FormControl fullWidth>
+                  <Autocomplete
+                    multiple
+                    id="tags-outlined"
+                    options={tempoOptions}
+                    getOptionLabel={(option) => option}
+                    filterSelectedOptions
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        variant="outlined"
+                        label="Tempo"
+                        {...register('tempo', { required: 'Required' })}
+                      />
+                    )}
+                  />
+                </FormControl>
+
+                {/* Original Key field */}
+                <FormControl fullWidth>
+                  <Autocomplete
+                    id="original-key"
+                    options={musicKeysOptions}
+                    getOptionLabel={(option) => option}
+                    filterSelectedOptions
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        variant="outlined"
+                        label="Original Key"
+                        {...register('originalKey', { required: 'Required' })}
+                      />
+                    )}
+                  />
+                </FormControl>
+
+                {/* Recommended Keys field */}
+                <FormControl fullWidth>
+                  <Autocomplete
+                    multiple
+                    id="recommended-keys"
+                    options={musicKeysOptions}
+                    getOptionLabel={(option) => option}
+                    filterSelectedOptions
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        variant="outlined"
+                        label="Recommended Keys"
+                        {...register('recommendedKeys')}
+                      />
+                    )}
+                  />
+                </FormControl>
+
+                {/* Year field */}
+                <TextField
+                  id="year"
+                  label="Year"
+                  type="number"
+                  defaultValue={2023}
+                  {...register('year')}
                 />
-              </FormControl>
 
-              {/* Recommended Keys field */}
-              <FormControl fullWidth>
-                <Autocomplete
-                  multiple
-                  id="recommended-keys"
-                  options={musicKeysOptions}
-                  getOptionLabel={(option) => option}
-                  filterSelectedOptions
-                  renderInput={(params) => (
-                    <TextField {...params} variant="outlined" label="Recommended Keys" />
-                  )}
+                {/* Code field */}
+                <TextField id="code" label="Code" {...register('code', { required: 'Required' })} />
+              </Stack>
+            </Box>
+
+            {/* column 2: Lyrics & Chords */}
+            <Box width="100%">
+              <Stack direction="column" spacing={2}>
+                <Typography variant="h4" sx={{ display: 'flex', alignItems: 'center' }} gap={1}>
+                  <LibraryMusic sx={{ color: PRIMARY_MAIN }} />
+                  Lyrics & Chords
+                </Typography>
+
+                <TextareaAutosize
+                  minRows={10}
+                  placeholder="Enter lyrics & chords here"
+                  {...register('chordLyrics')}
                 />
-              </FormControl>
-            </Stack>
-          </Box>
-
-          {/* column 2 */}
-          <Box width="100%">
-            <Stack direction="column" spacing={2}>
-              <Typography variant="h4" sx={{ display: 'flex', alignItems: 'center' }} gap={1}>
-                <LibraryMusic sx={{ color: PRIMARY_MAIN }} />
-                Lyrics & Chords
-              </Typography>
-
-              <TextareaAutosize minRows={10} placeholder="Enter lyrics & chords here" />
-            </Stack>
-          </Box>
-        </Stack>
-      </Box>
+              </Stack>
+            </Box>
+          </Stack>
+        </Box>
+      </form>
     </Container>
   );
 };

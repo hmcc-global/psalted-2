0699c3e6cf29ba7f49105a929b134da72cf849f4
Richie <richie.salim@Richie-2.local>
2023-12-08 17:42:00 +0800

    GH-24: purge lyrics rich text editor logic ~~~

--- ui/src/components/songsView/songsButtonsCard.tsx
+++ ui/src/components/songsView/songsButtonsCard.tsx
@@ -10,10 +10,10 @@ import {
   Stack,
   Chip,
   IconButton,
+  useMediaQuery,
 } from '@mui/material';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import VerticalSplitIcon from '@mui/icons-material/VerticalSplit';
 import { transposeChord } from './helpers';
 import SongsLyrics from './songsLyrics';
 
@@ -27,6 +27,8 @@ const SongsButtonCard = (props: SongsButtonCardProps) => {
   const [state, setState] = useState({ chord: false });
   const [isOpen, setIsOpen] = useState(false);
   const [count, setCount] = useState(0);
+  const [split, setSplit] = useState(1);
+  const isDesktop = useMediaQuery('(min-width:768px)')
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setIsOpen((isOpen) => !isOpen);
@@ -44,6 +46,11 @@ const SongsButtonCard = (props: SongsButtonCardProps) => {
     setCount(count - 1);
   };
 
+  const handleSplit = () => {
+    if (split < 3) setSplit(split + 1);
+    else setSplit(1);
+  };
+
   return (
     <>
       <Container>
@@ -55,12 +62,15 @@ const SongsButtonCard = (props: SongsButtonCardProps) => {
               borderRadius: 0,
               p: 0,
               width: '100%',
+              padding:"12px"
             }}
           >
             <Stack direction="row" spacing={{ base: '0', xs: '1vh', sm: '1.5vh', md: '2vh' }}>
               <Box
-                justifyContent={'left'}
-                width={{ base: '13vh', xs: '9h', sm: '11vh', md: '12vh' }}
+                justifyContent='left'
+                bgcolor="white"
+                padding="4px 12px"
+                borderRadius="4px"
               >
                 <FormGroup>
                   <FormControlLabel
@@ -70,13 +80,19 @@ const SongsButtonCard = (props: SongsButtonCardProps) => {
                   />
                 </FormGroup>
               </Box>
-              <Stack direction="row" spacing={{ base: '0', xs: '0', sm: '1px', md: '2px' }}>
+              <Stack
+                direction="row"
+                spacing={{ base: '0', xs: '0', sm: '1px', md: '2px' }}
+                bgcolor="white"
+                padding="4px 12px"
+                borderRadius="4px"
+              >
                 <Box bgcolor={'white'}>
                   <Typography alignItems={'center'} paddingTop="5px">
                     Key
                   </Typography>
                 </Box>
-                <Box bgcolor="primary.light">
+                <Box bgcolor="primary.lightest">
                   <IconButton color="primary" aria-label="down" onClick={handleDecrement}>
                     <KeyboardArrowDownIcon />
                   </IconButton>
@@ -93,28 +109,53 @@ const SongsButtonCard = (props: SongsButtonCardProps) => {
                   )}
                   color="primary"
                 />
-                <Box bgcolor="primary.light">
+                <Box bgcolor="primary.lightest">
                   <IconButton color="primary" aria-label="up" onClick={handleIncrement}>
                     <KeyboardArrowUpIcon />
                   </IconButton>
                 </Box>
               </Stack>
-              <Stack direction="row" spacing={1}>
+              <Stack
+                direction="row"
+                spacing={1}
+                bgcolor="white"
+                padding="4px 12px"
+                borderRadius="4px"
+                display={isDesktop ? "flex" : "none"}
+              >
                 <Box>
                   <Typography alignItems={'center'} paddingTop="5px">
                     Split
                   </Typography>
                 </Box>
-                <Box bgcolor="primary.light">
-                  <IconButton color="primary" aria-label="down">
-                    <VerticalSplitIcon />
-                  </IconButton>
+                <Box
+                  border="1px solid #FAFAFA"
+                  height="40px"
+                  width="40px"
+                  borderRadius="4px"
+                  onClick={handleSplit}
+                >
+                  <Stack
+                    direction="row"
+                    height="100%"
+                    width="100%"
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                    spacing={0.2}
+                  >
+                    {Array.from({ length: split }, (e, i) => {
+                      return (
+                        <Box bgcolor="primary.main" height="15px" width="6px" borderRadius="2px" />
+                      );
+                    })}
+                  </Stack>
                 </Box>
               </Stack>
             </Stack>
           </Box>
         </Box>
-        <SongsLyrics chordStatus={isOpen} changeKey={count} song={songs} />
+        <SongsLyrics chordStatus={isOpen} changeKey={count} song={songs} split={split} />
       </Container>
     </>
   );

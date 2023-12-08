e6dcf5b7d2a4d2774d7f17564e48ef48706b4965
Richie <richie.salim@Richie-2.local>
2023-12-08 17:42:00 +0800

    GH-24: purge lyrics rich text editor logic ~~~

--- ui/src/components/songsView/helpers.tsx
+++ ui/src/components/songsView/helpers.tsx
@@ -60,91 +60,92 @@ export const transposeChord = (originalKey: string, chord: string, transpose: nu
   });
 };
 
-export const parseNoChords = (a: string | undefined) => {
-  var tempStr: string = String(a).replaceAll('{', '\n{');
-  tempStr = tempStr.replaceAll('}', '}\n');
-  tempStr = tempStr.replace(/\[(.*?)\]/g, ' ');
-  tempStr = tempStr.replaceAll('[', ' ');
-  tempStr = tempStr.replaceAll(']', ' ');
-  let lyricsArray: Array<string>;
-  lyricsArray = tempStr.split('\n');
-  return lyricsArray;
-};
-
-export const parseWithChords = (
+export const parseLyrics = (
   originalKey: string | undefined,
   a: string | undefined,
-  transpose: number
+  transpose: number,
+  chordStatus: boolean
 ) => {
-  var tempStr: string = String(a).replaceAll('\n', ' \n ');
-  tempStr = tempStr.replaceAll('{', '\n{');
-  tempStr = tempStr.replaceAll('}', '}\n ');
+  if (chordStatus) {
+    var tempStr: string = String(a).replaceAll('\n', ' \n ');
+    tempStr = tempStr.replaceAll('{', '\n{');
+    tempStr = tempStr.replaceAll('}', '}\n ');
 
-  let lyricsArray: Array<string>;
-  lyricsArray = tempStr.split('\n');
+    let lyricsArray: Array<string>;
+    lyricsArray = tempStr.split('\n');
 
-  var beginIndex: number = 0;
-  var endIndex: number = 0;
-  var chordStr: string = '';
-  var newStr: string = '';
-  var prevEndIndex: number = 0;
-  var lineLength = lyricsArray.length;
-  var pass: boolean = false;
+    var beginIndex: number = 0;
+    var endIndex: number = 0;
+    var chordStr: string = '';
+    var newStr: string = '';
+    var prevEndIndex: number = 0;
+    var lineLength = lyricsArray.length;
+    var pass: boolean = false;
 
-  for (let index = 0; index < lineLength; index++) {
-    prevEndIndex = 0;
-    // Find if '[' or ']' exists
-    // Extract substring in between
-    // find the length of the substring
-    // push to the end of array - to update array length
-    // replace the [ ]
-    // change last array position to + 1 of current
-    //shift the other ones
-    while ((beginIndex = lyricsArray[index].indexOf('[')) > 0) {
-      endIndex = lyricsArray[index].indexOf(']');
-      chordStr = lyricsArray[index].substring(beginIndex, endIndex + 1);
-      for (let space = 0; space < beginIndex - prevEndIndex; space++) {
-        newStr = newStr.concat(' ');
+    for (let index = 0; index < lineLength; index++) {
+      prevEndIndex = 0;
+      // Find if '[' or ']' exists
+      // Extract substring in between
+      // find the length of the substring
+      // push to the end of array - to update array length
+      // replace the [ ]
+      // change last array position to + 1 of current
+      //shift the other ones
+      while ((beginIndex = lyricsArray[index].indexOf('[')) > 0) {
+        endIndex = lyricsArray[index].indexOf(']');
+        chordStr = lyricsArray[index].substring(beginIndex, endIndex + 1);
+        for (let space = 0; space < beginIndex - prevEndIndex; space++) {
+          newStr = newStr.concat(' ');
+        }
+        newStr = newStr + chordStr;
+
+        lyricsArray[index] = lyricsArray[index].replace('[', '<');
+        lyricsArray[index] = lyricsArray[index].replace(']', '>');
+        prevEndIndex = endIndex;
+        pass = true;
       }
-      newStr = newStr + chordStr;
 
-      lyricsArray[index] = lyricsArray[index].replace('[', '<');
-      lyricsArray[index] = lyricsArray[index].replace(']', '>');
-      prevEndIndex = endIndex;
-      pass = true;
+      // console.log(lyricsArray.length);
+      if (pass) {
+        lyricsArray[index] = lyricsArray[index].replace(/<([^>]+)>/g, ' ');
+        lyricsArray.push(newStr);
+        pass = false;
+        newStr = '';
+      } else {
+        lyricsArray.push(' ');
+      }
+    }
+    let lyricsArrayFinal: Array<string> = ['something'];
+    for (let i = 0; i < lyricsArray.length; i++) {
+      lyricsArrayFinal.push('');
     }
 
-    // console.log(lyricsArray.length);
-    if (pass) {
-      lyricsArray[index] = lyricsArray[index].replace(/<([^>]+)>/g, ' ');
-      lyricsArray.push(newStr);
-      pass = false;
-      newStr = '';
-    } else {
-      lyricsArray.push(' ');
+    for (let i = 0; i < lyricsArray.length / 2; i++) {
+      // lyrics
+      lyricsArray[i] = lyricsArray[i].trim();
+      lyricsArray[i] = lyricsArray[i].replace(/\s+/g, ' ');
+      lyricsArrayFinal[i * 2 + 1] = lyricsArray[i];
     }
-  }
-  let lyricsArrayFinal: Array<string> = ['something'];
-  for (let i = 0; i < lyricsArray.length; i++) {
-    lyricsArrayFinal.push('');
-  }
+    for (let i = 0; i < lyricsArray.length / 2; i++) {
+      // chords
+      lyricsArray[i + lyricsArray.length / 2] = transposeChord(
+        String(originalKey),
+        lyricsArray[i + lyricsArray.length / 2],
+        transpose
+      );
+      lyricsArrayFinal[i * 2] = lyricsArray[i + lyricsArray.length / 2];
+    }
 
-  for (let i = 0; i < lyricsArray.length / 2; i++) {
-    // lyrics
-    lyricsArray[i] = lyricsArray[i].trim();
-    lyricsArray[i] = lyricsArray[i].replace(/\s+/g, ' ');
-    lyricsArrayFinal[i * 2 + 1] = lyricsArray[i];
+    //console.log(lyricsArrayFinal);
+    return lyricsArrayFinal;
+  } else {
+    tempStr = String(a).replaceAll('{', '\n{');
+    tempStr = tempStr.replaceAll('}', '}\n');
+    tempStr = tempStr.replace(/\[(.*?)\]/g, ' ');
+    tempStr = tempStr.replaceAll('[', ' ');
+    tempStr = tempStr.replaceAll(']', ' ');
+    let lyricsArray: Array<string>;
+    lyricsArray = tempStr.split('\n');
+    return lyricsArray;
   }
-  for (let i = 0; i < lyricsArray.length / 2; i++) {
-    // chords
-    lyricsArray[i + lyricsArray.length / 2] = transposeChord(
-      String(originalKey),
-      lyricsArray[i + lyricsArray.length / 2],
-      transpose
-    );
-    lyricsArrayFinal[i * 2] = lyricsArray[i + lyricsArray.length / 2];
-  }
-
-  //console.log(lyricsArrayFinal);
-  return lyricsArrayFinal;
 };

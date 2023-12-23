9585bd9a8a66bf22d8a067b7c6bcb47615ce58ed
Tamara Yustian <tamarayustian@gmail.com>
2023-12-23 18:54:59 +0800

    GH-24: Create a list api

--- server/src/controllers/song-option.controllers.ts
+++ server/src/controllers/song-option.controllers.ts
@@ -5,7 +5,7 @@ import { SongOptionDocument } from '../types/song-option.types';
 const sendResponse = (
   res: Response,
   statusCode: number,
-  payload: SongOptionDocument[] | SongOptionDocument | string
+  payload: SongOptionDocument[] | SongOptionDocument | string | any
 ) => {
   return res.status(statusCode).json(payload);
 };
@@ -65,4 +65,24 @@ const getSongOption: RequestHandler = async (req: Request, res: Response): Promi
   }
 };
 
-export { createSongOption, getSongOption };
+const getSongOptionList: RequestHandler = async (_req: Request, res: Response): Promise<void> => {
+  try {
+    const data: SongOptionDocument[] = await SongOption.find({ isDeleted: false });
+
+    if (data) {
+      const result: any = {};
+
+      for (const row of data) {
+        result[row.category] = row.list;
+      }
+
+      sendResponse(res, 200, result);
+    } else {
+      sendResponse(res, 404, 'Song options not found');
+    }
+  } catch (error: any) {
+    sendResponse(res, 500, error?.message);
+  }
+};
+
+export { createSongOption, getSongOption, getSongOptionList };
--- server/src/routes/song-option.routes.ts
+++ server/src/routes/song-option.routes.ts
@@ -1,9 +1,14 @@
-import { getSongOption, createSongOption } from '../controllers/song-option.controllers';
+import {
+  getSongOption,
+  createSongOption,
+  getSongOptionList,
+} from '../controllers/song-option.controllers';
 import { Router } from 'express';
 
 const songOptionRouter = Router();
 
 songOptionRouter.post('/create', createSongOption);
 songOptionRouter.post('/get', getSongOption);
+songOptionRouter.get('/list', getSongOptionList);
 
 export default songOptionRouter;

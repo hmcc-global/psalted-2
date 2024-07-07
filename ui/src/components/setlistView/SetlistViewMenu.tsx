d1e8a1b883f54490b64dc5bc3bd8f33906d21995
Midnite1212 <richie.salim@bowtie.com.hk>
2024-07-07 19:45:36 +0800

    GH-66: finish menu for the view

--- ui/src/components/setlistView/SetlistViewMenu.tsx
+++ ui/src/components/setlistView/SetlistViewMenu.tsx
@@ -1,4 +1,16 @@
-import { Menu, MenuItem } from '@mui/material';
+import {
+  IconButton,
+  ListItemIcon,
+  ListItemText,
+  Menu,
+  MenuItem,
+  Snackbar,
+  useTheme,
+} from '@mui/material';
+import ShareIcon from '@mui/icons-material/Share';
+import EditIcon from '@mui/icons-material/Edit';
+import { useState } from 'react';
+import CloseIcon from '@mui/icons-material/Close';
 
 type Props = {
   anchorEl: HTMLElement | null;
@@ -7,15 +19,55 @@ type Props = {
 };
 const SetlistViewMenu = (props: Props) => {
   const { onClose } = props;
+  const [open, setOpen] = useState(false);
+  const theme = useTheme();
+
+  const handleCloseSnackbar = (event: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
 
   const handleShare = () => {
+    setOpen(true);
+    navigator.clipboard.writeText(window.location.href);
+    onClose();
+  };
+
+  const handleEdit = () => {
     onClose();
   };
 
   return (
-    <Menu {...props}>
-      <MenuItem onClick={handleShare}>Share Setlist</MenuItem>
-    </Menu>
+    <>
+      <Menu {...props}>
+        <MenuItem onClick={handleShare}>
+          <ListItemIcon>
+            <ShareIcon style={{ color: theme.palette.primary.light }} fontSize="small" />
+          </ListItemIcon>
+          <ListItemText>Share Setlist</ListItemText>
+        </MenuItem>
+        <MenuItem onClick={handleEdit}>
+          <ListItemIcon>
+            <EditIcon style={{ color: theme.palette.primary.light }} fontSize="small" />
+          </ListItemIcon>
+          <ListItemText>Edit Setlist (when user access is done)</ListItemText>
+        </MenuItem>
+      </Menu>
+      <Snackbar
+        open={open}
+        autoHideDuration={5000}
+        onClose={handleCloseSnackbar}
+        message="Link copied to clipboard!"
+        action={
+          <IconButton size="small" aria-label="close" color="inherit" onClick={handleCloseSnackbar}>
+            <CloseIcon fontSize="small" />
+          </IconButton>
+        }
+      />
+    </>
   );
 };
 

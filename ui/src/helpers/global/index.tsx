53724f9ec335c407fd6ceb86bc7f082a3088496d
Yoo Yuen Yau <yuenyau989@hotmail.com>
2024-07-27 18:47:54 +0800

    GH-71: Formatting

--- ui/src/helpers/global/index.tsx
+++ ui/src/helpers/global/index.tsx
@@ -1,3 +1,3 @@
-export {default as getSongsGlobal} from './getSongsGlobal';
-export {default as getSetlistsGlobal} from './getSetlistsGlobal';
-export {default as getFoldersGlobal} from './getFoldersGlobal';
+export { default as getSongsGlobal } from './getSongsGlobal';
+export { default as getSetlistsGlobal } from './getSetlistsGlobal';
+export { default as getFoldersGlobal } from './getFoldersGlobal';

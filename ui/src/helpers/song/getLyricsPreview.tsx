64b62e391d5eb6dab57ca6f0abb41ecf7c5356ee
Yoo Yuen Yau <yuenyau989@hotmail.com>
2024-08-25 03:55:35 +0800

    GH-87: Added custom button theming

--- ui/src/helpers/song/getLyricsPreview.tsx
+++ ui/src/helpers/song/getLyricsPreview.tsx
@@ -3,7 +3,10 @@ import { Fragment } from 'react';
 const getLyricsPreview = (lyrics: string) => {
   //get the first verse only
   const firstVerse = lyrics.split('}')[1];
-  const cleanLyrics = firstVerse?.split('{')[0]?.replace(/\[.*?\]/g, '').trim();
+  const cleanLyrics = firstVerse
+    ?.split('{')[0]
+    ?.replace(/\[.*?\]/g, '')
+    .trim();
   if (cleanLyrics) {
     return cleanLyrics.split('\n').map((line, i) => (
       <Fragment key={i}>

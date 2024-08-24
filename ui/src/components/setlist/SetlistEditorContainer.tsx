0a7746cac85f82b50cea494b427c637994bcce0b
Yoo Yuen Yau <yuenyau989@hotmail.com>
2024-08-25 04:02:57 +0800

    GH-87: Setlist editor 95% completed, group update/create API left

--- ui/src/components/setlist/SetlistEditorContainer.tsx
+++ ui/src/components/setlist/SetlistEditorContainer.tsx
@@ -1,6 +1,6 @@
-import { SetlistEditorFields, SetlistEditorProps } from '../../types/setlist.types';
-import { SongCardProps, SongSearchFilter } from '../../types/song.types';
-import { Info, MoreVert, MusicNote, Search } from '@mui/icons-material';
+import { SetlistEditorFields, SetlistEditorProps, SetlistFolder } from '../../types/setlist.types';
+import { SongSchema, SongSearchFilter, SongSetlistSchema } from '../../types/song.types';
+import { Info, MusicNote, Search, QueueMusic, AddCircleOutline } from '@mui/icons-material';
 import {
   Alert,
   AlertTitle,
@@ -8,100 +8,192 @@ import {
   Button,
   Container,
   Fade,
+  FormControl,
   IconButton,
   InputBase,
   Snackbar,
   Stack,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
   TextField,
-  Toolbar,
   Typography,
   useMediaQuery,
 } from '@mui/material';
-import { LocalizationProvider, DateField } from '@mui/x-date-pickers';
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import { LocalizationProvider, DateField, DatePicker } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs, { Dayjs } from 'dayjs';
 import axios from 'axios';
-import { FC, ReactElement, useCallback, useEffect, useState } from 'react';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { FC, useCallback, useEffect, useState } from 'react';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import SetlistSongCard from './SetlistSongCard';
 import HeaderWithIcon from '../custom/HeaderWithIcon';
+import PageHeader from '../navigation/PageHeader';
+import { useFolders, useSongs } from '../../helpers/customHooks';
+import SetlistSongsTable from './SetlistSongTable';
+import AutocompleteInput from '../custom/AutocompleteInput';
 
-const SetlistEditorContainer: FC<SetlistEditorProps> = ({ actionOnEditor }) => {
+const SetlistEditorContainer: FC<SetlistEditorProps> = () => {
   const isDesktop = useMediaQuery('(min-width: 768px)');
   const navigate = useNavigate();
 
+  const [action, setAction] = useState<string>('new');
+  const paths: string[] = window.location.pathname.split('/');
+  useEffect(() => {
+    if (paths.includes('edit')) {
+      setAction('edit');
+      setSetlistId(paths[paths.length - 1]);
+    }
+  }, [paths]);
+
+  const allSongs = useSongs() as SongSchema[];
+  const allFolders = useFolders() as SetlistFolder[];
+
   // STATES
-  const [date, setDate] = useState<Date | null>(null);
+  const [date, setDate] = useState<Dayjs | null>(null);
   const [search, setSearch] = useState<string>('');
-  const [allSongs, setAllSongs] = useState<SongCardProps[]>([]);
-  const [songResults, setSongResults] = useState<SongCardProps[]>([]);
+  const [songResults, setSongResults] = useState<SongSchema[]>([]);
   const [filterData, setFilterData] = useState<SongSearchFilter>();
-  const [addedSongs, setAddedSongs] = useState<string[]>([]);
   const [showDetails, setShowDetails] = useState<boolean>(true);
 
   const [successSnackbarOpen, setSuccessSnackbarOpen] = useState<boolean>(false);
   const [invalidSetlist, setInvalidSetlist] = useState<string>('');
 
+  const [setlist, setSetlist] = useState<SetlistEditorFields>({} as SetlistEditorFields);
+  const [setlistId, setSetlistId] = useState<string>('');
+
+  const [addedSongList, setAddedSongList] = useState<SongSetlistSchema[]>([]);
+  const [folderList, setFolderList] = useState<string[]>([]);
+  const [folderOptions, setFolderOptions] = useState<SetlistFolder[]>([]);
+
   // FORM HANDLER
-  const { register, handleSubmit, formState } = useForm<SetlistEditorFields>();
+  const { handleSubmit, formState, control, reset, register } = useForm<SetlistEditorFields>();
   const { errors } = formState;
 
-  // editor mode is either NEW or EDIT. default is NEW
-  const editorMode = (actionOnEditor: string): ReactElement => {
-    return (
-      <Typography variant="h3" color="white" sx={{ flexGrow: 1 }} gap={1} mx={2}>
-        {actionOnEditor ? actionOnEditor : 'New'} Setlist
-      </Typography>
-    );
-  };
+  const getSetlist = useCallback(async () => {
+    if (setlistId === '') return;
+
+    try {
+      const { data, status } = await axios.get(`/api/setlists/get`, {
+        params: {
+          id: setlistId,
+        },
+      });
+      if (status === 200) {
+        setSetlist(data);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }, [setlistId]);
+
+  useEffect(() => {
+    getSetlist();
+  }, [getSetlist, setlistId]);
+
+  useEffect(() => {
+    if (setlist && Object.keys(setlist).length > 0) {
+      reset({
+        name: setlist.name,
+        date: dayjs(setlist.date),
+        songs: setlist.songs,
+      });
+      setDate(dayjs(setlist.date));
+      setAddedSongList(setlist.songs);
+      setFolderList(
+        folderOptions
+          .filter((folder) => setlist.groupIds.includes(folder._id))
+          .map((folder) => folder.groupName)
+      );
+    }
+  }, [setlist, folderOptions, reset]);
 
   const getSongResults = useCallback(async () => {
-    try {
-      const { data, status } = await axios.get('/api/songs/get');
-      if (status === 200) {
-        setAllSongs(data);
-
-        // TODO: filter logic with allSongs, setFilterData, and setShowDetails
-        if (search !== '') {
-          const songs: SongCardProps[] = data;
-
-          const filteredSongs = songs.filter((song) => song.title.includes(search));
-          setSongResults(filteredSongs);
-        } else setSongResults(data);
-      }
-    } catch (error) {}
-  }, [search]);
+    if (search !== '') {
+      const filteredSongs = allSongs.filter((song) =>
+        song.title.toLowerCase().includes(search.toLowerCase())
+      );
+      setSongResults(filteredSongs);
+    } else setSongResults(allSongs);
+  }, [search, allSongs]);
 
   useEffect(() => {
     getSongResults();
   }, [getSongResults]);
 
+  useEffect(() => {
+    if (allFolders) {
+      setFolderOptions(allFolders);
+    }
+  }, [allFolders]);
+
   // add the id to the array of clicked items if it doesn't exist but if it does exist remove it
   // this makes sure that double clicking on an item brings it back to normal
   // change <Add /> to <Check /> at "id"
   const handleAddSong = (id: string) => {
-    let result = addedSongs.includes(id)
-      ? addedSongs.filter((add) => add != id)
-      : [...addedSongs, id];
-    setAddedSongs(result);
-  };
+    const toRemove = addedSongList.find((song) => song._id === id);
 
-  // To render the songs that are added to setlist
-  const addedSongList = songResults.filter((song) => addedSongs.includes(song._id));
+    if (toRemove) {
+      const updatedSongList = [...addedSongList];
+      updatedSongList.splice(addedSongList.indexOf(toRemove), 1);
+
+      updatedSongList.forEach((song, i) => {
+        if (song.sequence! > toRemove.sequence!) {
+          updatedSongList[i] = { ...song, sequence: song.sequence! - 1 };
+        }
+      });
+
+      setAddedSongList(updatedSongList);
+    } else {
+      const songResult = songResults.find((song) => song._id === id);
+      if (!songResult) return;
+
+      const setlistSong: SongSetlistSchema = {
+        ...songResult,
+        key: songResult.originalKey,
+        sequence: addedSongList.length + 1,
+      };
+
+      setAddedSongList([...addedSongList, setlistSong]);
+    }
+  };
 
   const handleSaveSetlist: SubmitHandler<SetlistEditorFields> = async (data) => {
     try {
-      const payload = await axios.post('/api/setlists/create', {
-        name: data.name,
-        date: date,
-        songs: addedSongList,
-      });
+      let payload;
+      if (action === 'edit') {
+        payload = await axios.put(`/api/setlists/update`, {
+          id: setlistId,
+          name: data.name,
+          date: date ? date.toDate() : new Date(''),
+          songs: addedSongList,
+          groupIds: folderOptions
+            .filter((folder) => folderList.includes(folder.groupName))
+            .map((folder) => folder._id),
+        });
+      } else {
+        payload = await axios.post('/api/setlists/create', {
+          name: data.name,
+          date: date ? date.toDate() : new Date(''),
+          songs: addedSongList,
+          groupIds: folderOptions
+            .filter((folder) => folderList.includes(folder.groupName))
+            .map((folder) => folder._id),
+          createdBy: '',
+        });
+      }
+
+      // folderList.forEach((folder) => {
+      //   const folderToUpdate = folderOptions.find((f) => f.groupName === folder);
+
+      //   if (folderToUpdate) {
+      //     const payload = axios.post('/api/group/update', {
+      //       _id: folderToUpdate._id,
+      //       setlistIds: [...folderToUpdate.setlistIds, setlistId],
+      //     });
+      //   } else {
+
+      //   }
+
+      // })
 
       if (payload.status === 200) {
         setInvalidSetlist('');
@@ -125,7 +217,7 @@ const SetlistEditorContainer: FC<SetlistEditorProps> = ({ actionOnEditor }) => {
   };
 
   return (
-    <Container sx={{ pt: '5em' }}>
+    <Container sx={{ py: '1rem', px: '2rem', height: '100%', minWidth: '100%', overflow: 'auto' }}>
       <Box>
         {/* Error message */}
         {invalidSetlist ? (
@@ -149,160 +241,123 @@ const SetlistEditorContainer: FC<SetlistEditorProps> = ({ actionOnEditor }) => {
         </Snackbar>
 
         <form onSubmit={handleSubmit(handleSaveSetlist)}>
-          <Toolbar>
-            {editorMode(actionOnEditor)}
-            <Button
-              type={'submit'}
-              color="secondary"
-              variant="contained"
-              sx={{
-                mr: 1,
-                textTransform: 'none',
-                borderRadius: '100px',
-                px: 3,
-              }}
-            >
-              Save
-            </Button>
-            <Button
-              color={'secondary'}
-              sx={{
-                textTransform: 'none',
-                borderRadius: '100px',
-                border: 1,
-                px: 2,
-              }}
-              onClick={() => navigate('/setlist')}
-            >
-              Cancel
-            </Button>
-          </Toolbar>
+          <PageHeader
+            title={action === 'edit' ? 'Edit Setlist' : 'New Setlist'}
+            icon={<QueueMusic />}
+            actionButtons={
+              <Stack direction="row">
+                <Button
+                  type={'submit'}
+                  color="secondary"
+                  variant="contained"
+                  sx={{
+                    mr: 1,
+                    textTransform: 'none',
+                    borderRadius: '100px',
+                    px: 3,
+                  }}
+                >
+                  Save
+                </Button>
+                <Button
+                  color={'secondary'}
+                  sx={{
+                    textTransform: 'none',
+                    borderRadius: '100px',
+                    border: 1,
+                    px: 2,
+                  }}
+                  onClick={() => navigate('/sosetlistng')}
+                >
+                  Cancel
+                </Button>
+              </Stack>
+            }
+          />
 
-          <Stack direction="row" spacing={10}>
+          <Stack direction="row" spacing={10} my={'24px'} width={'100%'}>
             {/* setlist details and info */}
-            <Box display="flex" justifyContent="center" width={'35vw'}>
+            <Box display="flex" justifyContent="center" width={'40vw'}>
               <Stack direction="column" spacing={2} width={'100%'}>
                 <HeaderWithIcon
                   Icon={Info}
                   headerText="Details"
                   headerVariant="h3"
-                  iconColor="primary.light"
+                  iconColor="secondary.main"
+                  headerColor={'secondary.main'}
                 />
-
                 {/* Setlist Name Field */}
-                <TextField
-                  id="name"
-                  label="Setlist Name"
-                  error={!!errors.name}
-                  helperText={errors?.name?.message}
-                  {...register('name', { required: 'Required' })}
+                <Controller
+                  name="name"
+                  control={control}
+                  defaultValue=""
+                  render={({ field }) => (
+                    <TextField
+                      id="name"
+                      label="Setlist Name"
+                      error={!!errors.name}
+                      helperText={errors?.name?.message}
+                      {...field}
+                    />
+                  )}
                 />
 
                 {/* Date Field */}
-                <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale="en-us">
-                  <DateField label="Date" value={date} onChange={(newValue) => setDate(newValue)} />
+                <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={'en'}>
+                  <DatePicker
+                    label="Date"
+                    value={date}
+                    onChange={(newValue) => {
+                      setDate(newValue);
+                      console.log(newValue);
+                    }}
+                  />
                 </LocalizationProvider>
 
                 {/* TODO: Add to Folder */}
-                <TextField label="Add to Folder (Optional)" />
+                <FormControl fullWidth>
+                  <AutocompleteInput
+                    id="folders"
+                    options={folderOptions.map((folder) => folder.groupName)}
+                    label="Folders"
+                    autoComplete="folders"
+                    value={folderList}
+                    onChange={(_, newValue) => {
+                      setFolderList(newValue as string[]);
+                    }}
+                    register={register}
+                    multiple
+                  />
+                </FormControl>
 
                 {/* Added Songs List */}
                 <Box sx={{ pt: 3 }}>
                   <Stack direction="column" spacing={1}>
                     {/* song list header */}
-                    <Stack
-                      direction="row"
-                      spacing={1}
-                      alignItems="center"
-                      sx={{ color: 'primary.light' }}
-                    >
-                      <MusicNote />
-                      <Typography variant="h3" alignItems="center">
-                        Song List
-                      </Typography>
-                    </Stack>
-
-                    <TableContainer>
-                      <Table>
-                        <TableHead>
-                          <TableRow>
-                            <TableCell>#</TableCell>
-                            <TableCell>Song Title</TableCell>
-                            <TableCell>Key</TableCell>
-                            <TableCell />
-                          </TableRow>
-                        </TableHead>
-                        <TableBody>
-                          {addedSongs.length > 0 ? (
-                            addedSongList.map((song, i) => (
-                              <TableRow key={song._id} sx={{ '& td': { border: 0 } }}>
-                                {/* reorder icon */}
-
-                                {/* number */}
-                                <TableCell width="10%">{i + 1}</TableCell>
-
-                                <TableCell width="55%">
-                                  <Typography variant="h2">{song.title}</Typography>
-                                  <Typography variant="subtitle1" sx={{ color: 'secondary.light' }}>
-                                    {song.artist}
-                                  </Typography>
-                                </TableCell>
-
-                                {/* key for song */}
-                                <TableCell width="15%">
-                                  <Box
-                                    alignContent="center"
-                                    sx={{
-                                      height: '45px',
-                                      width: '45px',
-                                      backgroundColor: 'primary.main',
-                                      borderRadius: '50%',
-                                    }}
-                                  >
-                                    <Typography color="secondary" variant="h3" align="center">
-                                      {song.originalKey}
-                                    </Typography>
-                                  </Box>
-                                </TableCell>
-
-                                {/* delete button */}
-                                <TableCell width="10%">
-                                  <IconButton>
-                                    <MoreVert color="secondary" />
-                                  </IconButton>
-                                </TableCell>
-                              </TableRow>
-                            ))
-                          ) : (
-                            <TableRow>
-                              <TableCell width="10%" sx={{ borderBottom: 0 }} />
-                              <TableCell width="55%" sx={{ borderBottom: 0 }}>
-                                <Typography
-                                  variant="subtitle1"
-                                  color="secondary.main"
-                                  align="center"
-                                  sx={{ pt: 2 }}
-                                >
-                                  No Songs Added
-                                </Typography>
-                              </TableCell>
-                            </TableRow>
-                          )}
-                        </TableBody>
-                      </Table>
-                    </TableContainer>
+                    <HeaderWithIcon
+                      Icon={MusicNote}
+                      headerText="Song List"
+                      headerVariant="h3"
+                      iconColor="secondary.main"
+                      headerColor={'secondary.main'}
+                    />
+                    <SetlistSongsTable songList={addedSongList} setSongList={setAddedSongList} />
                   </Stack>
                 </Box>
               </Stack>
             </Box>
 
             {/* search for songs to add */}
-            <Box display="flex" justifyContent="center" width={'35vw'}>
+            <Box display="flex" justifyContent="center" width={'40vw'}>
               <Stack direction="column" spacing={2} width={'100%'}>
                 {/* header */}
-                <Typography variant="h4" sx={{ color: 'primary.light' }}>
-                  Search to Add Songs
-                </Typography>
+                <HeaderWithIcon
+                  Icon={AddCircleOutline}
+                  headerText="Search to Add Songs"
+                  headerVariant="h3"
+                  iconColor="secondary.main"
+                  headerColor={'secondary.main'}
+                />
 
                 {/* search bar */}
                 <Stack
@@ -345,7 +400,7 @@ const SetlistEditorContainer: FC<SetlistEditorProps> = ({ actionOnEditor }) => {
                       filterData={filterData}
                       isDesktop={isDesktop}
                       handleAddSong={handleAddSong}
-                      addedSongs={addedSongs}
+                      addedSongs={addedSongList.map((song) => song._id)}
                     />
                   ))
                 ) : (
